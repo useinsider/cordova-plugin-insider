231032c0f0d8a4bbc2a383df7b8db5b74c160a94
orkunsoylu <soyluorkun@gmail.com>
2019-04-12 14:48:03 +0300

    switched all switch to if statement

--- src/android/InsiderPlugin.java
+++ src/android/InsiderPlugin.java
@@ -54,147 +54,141 @@ public class InsiderPlugin extends CordovaPlugin {
             return false;
         }
         try {
-            switch (action) {
-                case "initWithAppGroup":
-                case "init":
-                    break;
-                case "setCustomAttributeWithString":
-                    Insider.Instance.setCustomAttributeWithString(args.getString(0), args.getString(1));
-                    break;
-                case "setCustomAttributeWithDouble":
-                    Insider.Instance.setCustomAttributeWithDouble(args.getString(0), args.getDouble(1));
-                    break;
-                case "setCustomAttributeWithBoolean":
-                    Insider.Instance.setCustomAttributeWithBoolean(args.getString(0), args.getBoolean(1));
-                    break;
-                case "setCustomAttributeWithDate":
-                    DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
-                    Insider.Instance.setCustomAttributeWithDate(args.getString(0), dateFormatter.parse(args.getString(1)));
-                    break;
-                case "setCustomAttributeWithArray":
-                    JSONArray attributeJSONArray = args.getJSONArray(1);
-                    String[] attributes = new String[attributeJSONArray.length()];
-                    for (int i = 0; i < attributeJSONArray.length(); i++) {
-                        attributes[i] = attributeJSONArray.getString(i);
-                    }
-                    Insider.Instance.setCustomAttributeWithArray(args.getString(0), attributes);
-                    break;
-                case "setCustomAttributes":
-                    JSONObject attrJson = new JSONObject(args.getString(0));
-                    HashMap<String, Object> attrMap = new HashMap<String,Object>();
-                    Iterator<?> keys = attrJson.keys();
-                    while (keys.hasNext()) {
+            if (action.equals("initWithAppGroup") || action.equals("init")) {
+                return true;
+            } else if (action.equals("setCustomAttributeWithString")) {
+                Insider.Instance.setCustomAttributeWithString(args.getString(0), args.getString(1));
+                return true;
+            } else if (action.equals("setCustomAttributeWithDouble")) {
+                Insider.Instance.setCustomAttributeWithDouble(args.getString(0), args.getDouble(1));
+                return true;
+            } else if (action.equals("setCustomAttributeWithBoolean")) {
+                Insider.Instance.setCustomAttributeWithBoolean(args.getString(0), args.getBoolean(1));
+                return true;
+            } else if (action.equals("setCustomAttributeWithDate")) {
+                DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+                Insider.Instance.setCustomAttributeWithDate(args.getString(0), dateFormatter.parse(args.getString(1)));
+                return true;
+            } else if (action.equals("setCustomAttributeWithArray")) {
+                JSONArray attributeJSONArray = args.getJSONArray(1);
+                String[] attributes = new String[attributeJSONArray.length()];
+                for (int i = 0; i < attributeJSONArray.length(); i++) {
+                    attributes[i] = attributeJSONArray.getString(i);
+                }
+                Insider.Instance.setCustomAttributeWithArray(args.getString(0), attributes);
+                return true;
+            } else if (action.equals("setCustomAttributes")) {
+                JSONObject attrJson = new JSONObject(args.getString(0));
+                HashMap<String, Object> attrMap = new HashMap<String,Object>();
+                Iterator<?> keys = attrJson.keys();
+                while (keys.hasNext()) {
                     String key = (String) keys.next();
                     attrMap.put(key, attrJson.get(key));
-                    }
-                    Insider.Instance.setCustomAttributes(attrMap);
-                    break;
-                case "unsetCustomAttribute":
-                    Insider.Instance.unsetCustomAttribute(args.getString(0));
-                    break;
-                case "setUserIdentifier":
-                    Insider.Instance.setUserIdentifier(args.getString(0));
-                    break;
-                case "unsetUserIdentifier":
-                    Insider.Instance.unsetUserIdentifier();
-                    break;
-                case "setPushEnabled":
-                    Insider.Instance.setPushEnabled(args.getBoolean(0));
-                    break;
-                case "setLocationEnabled":
-                    Insider.Instance.setLocationEnabled(args.getBoolean(0));
-                    break;
-                case "tagEvent":
-                    Insider.Instance.tagEvent(args.getString(0));
-                    break;
-                case "tagEventWithParameters":
-                    JSONObject paramJson = new JSONObject(args.getString(1));
-                    HashMap<String, Object> paramMap = new HashMap<String,Object>();
-                    Iterator<?> paramsKeys = paramJson.keys();
-                    while (paramsKeys.hasNext()) {
-                        String key = (String) paramsKeys.next();
-                        paramMap.put(key, paramJson.get(key));
-                    }
-                    Insider.Instance.tagEventWithParameters(args.getString(0), paramMap);
-                    break;
-                case "getStringWithName":
-                    ContentOptimizerVariableType stringVariableDataType = getDataType(args.getString(2));
-                    String optimizedString = Insider.Instance.getStringWithName(args.getString(0), args.getString(1), stringVariableDataType);
-                    if (optimizedString != null && optimizedString.length() > 0) {
-                        callbackSuccess(callbackContext, optimizedString);
-                    }
-                    break;
-                case "getIntWithName":
-                    ContentOptimizerVariableType intVariableDataType = getDataType(args.getString(2));
-                    int optimizedInteger = Insider.Instance.getIntWithName(args.getString(0), args.getInt(1), intVariableDataType);
-                    callbackSuccess(callbackContext, optimizedInteger);
-                    break;
-                case "getBoolWithName":
-                    ContentOptimizerVariableType boolVariableDataType = getDataType(args.getString(2));
-                    boolean optimizedBoolean = Insider.Instance.getBoolWithName(args.getString(0), args.getBoolean(1), boolVariableDataType);
-                    callbackSuccess(callbackContext, optimizedBoolean);
-                    break;
-                case "refreshDeviceToken":
-                    Insider.Instance.refreshDeviceToken();
-                    break;
-                case "tagProduct":
-                    Insider.Instance.tagProduct(args.getString(0));
-                    break;
-                case "trackPurchasedItems":
-                    Insider.Instance.trackPurchasedItems(
-                            args.getString(0),
-                            args.getString(1),
-                            args.getString(2),
-                            args.getString(3),
-                            args.getString(4),
-                            args.getDouble(5),
-                            args.getString(6)
-                    );
-                    break;
-                case "itemAddedToCart":
-                    Insider.Instance.itemAddedToCart(
-                        args.getString(0),
-                        args.getString(1),
-                        args.getDouble(2),
-                        args.getString(3),
-                        args.getString(4)
-                    );
-                    break;
-                case "itemRemovedFromCart":
-                    Insider.Instance.itemRemovedFromCart(
-                            args.getString(0)
-                    );
-                    break;
-                case "cartCleared":
-                    Insider.Instance.cartCleared();
-                    break;
-                case "getDeepLinkData":
-                    Object deepLinkData = Insider.Instance.getDeepLinkData(args.getString(0));
-                    if (deepLinkData != null && deepLinkData.toString().length() > 0) {
-                        callbackSuccess(callbackContext, deepLinkData.toString());
-                    }
-                    break;
-                case "cleanView":
-                    Insider.Instance.cleanView(activity);
-                    break;
-                default:
-                    return false;
+                }
+                Insider.Instance.setCustomAttributes(attrMap);
+                return true;
+            } else if (action.equals("unsetCustomAttribute")) {
+                Insider.Instance.unsetCustomAttribute(args.getString(0));
+                return true;
+            } else if (action.equals("setUserIdentifier")) {
+                Insider.Instance.setUserIdentifier(args.getString(0));
+                return true;
+            } else if (action.equals("unsetUserIdentifier")) {
+                Insider.Instance.unsetUserIdentifier();
+                return true;
+            } else if (action.equals("setPushEnabled")) {
+                Insider.Instance.setPushEnabled(args.getBoolean(0));
+                return true;
+            } else if (action.equals("setLocationEnabled")) {
+                Insider.Instance.setLocationEnabled(args.getBoolean(0));
+                return true;
+            } else if (action.equals("tagEvent")) {
+                Insider.Instance.tagEvent(args.getString(0));
+                return true;
+            } else if (action.equals("tagEventWithParameters")) {
+                JSONObject paramJson = new JSONObject(args.getString(1));
+                HashMap<String, Object> paramMap = new HashMap<String,Object>();
+                Iterator<?> paramsKeys = paramJson.keys();
+                while (paramsKeys.hasNext()) {
+                    String key = (String) paramsKeys.next();
+                    paramMap.put(key, paramJson.get(key));
+                }
+                Insider.Instance.tagEventWithParameters(args.getString(0), paramMap);
+                return true;
+            } else if (action.equals("getStringWithName")) {
+                ContentOptimizerVariableType stringVariableDataType = getDataType(args.getString(2));
+                String optimizedString = Insider.Instance.getStringWithName(args.getString(0), args.getString(1), stringVariableDataType);
+                if (optimizedString != null && optimizedString.length() > 0) {
+                    callbackSuccess(callbackContext, optimizedString);
+                }
+                return true;
+            } else if (action.equals("getIntWithName")) {
+                ContentOptimizerVariableType intVariableDataType = getDataType(args.getString(2));
+                int optimizedInteger = Insider.Instance.getIntWithName(args.getString(0), args.getInt(1), intVariableDataType);
+                callbackSuccess(callbackContext, optimizedInteger);
+                return true;
+            } else if (action.equals("getBoolWithName")) {
+                ContentOptimizerVariableType boolVariableDataType = getDataType(args.getString(2));
+                boolean optimizedBoolean = Insider.Instance.getBoolWithName(args.getString(0), args.getBoolean(1), boolVariableDataType);
+                callbackSuccess(callbackContext, optimizedBoolean);
+                return true;
+            } else if (action.equals("refreshDeviceToken")) {
+                Insider.Instance.refreshDeviceToken();
+                return true;
+            } else if (action.equals("tagProduct")) {
+                Insider.Instance.tagProduct(args.getString(0));
+                return true;
+            } else if (action.equals("trackPurchasedItems")) {
+                Insider.Instance.trackPurchasedItems(
+                    args.getString(0),
+                    args.getString(1),
+                    args.getString(2),
+                    args.getString(3),
+                    args.getString(4),
+                    args.getDouble(5),
+                    args.getString(6)
+                );
+                return true;
+            } else if (action.equals("itemAddedToCart")) {
+                Insider.Instance.itemAddedToCart(
+                    args.getString(0),
+                    args.getString(1),
+                    args.getDouble(2),
+                    args.getString(3),
+                    args.getString(4)
+                );
+                return true;
+            } else if (action.equals("itemRemovedFromCart")) {
+                Insider.Instance.itemRemovedFromCart(
+                    args.getString(0)
+                );
+                return true;
+            } else if (action.equals("cartCleared")) {
+                Insider.Instance.cartCleared();
+                return true;
+            } else if (action.equals("getDeepLinkData")) {
+                Object deepLinkData = Insider.Instance.getDeepLinkData(args.getString(0));
+                if (deepLinkData != null && deepLinkData.toString().length() > 0) {
+                    callbackSuccess(callbackContext, deepLinkData.toString());
+                }
+                return true;
+            } else if (action.equals("cleanView")) {
+                Insider.Instance.cleanView(activity);
+                return true;
+            } else {
+                return false;
             }
-            return true;
         } catch (Exception e) {
             Insider.Instance.putException(e);
         }
-        return false;
+        return false;  
     }
-
+             
     private static ContentOptimizerVariableType getDataType(String dataType){
-        switch (dataType) {
-          case "Content":
-             return ContentOptimizerVariableType.InsiderVariableTypeContent;
-          case "Element":
+        if (dataType.equals("Content")) {
+            return ContentOptimizerVariableType.InsiderVariableTypeContent;
+        } else {
             return ContentOptimizerVariableType.InsiderVariableTypeElement;
-          default:
-            return null;
         }
     }
 
